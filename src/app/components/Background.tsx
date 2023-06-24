8c588856f39e42d337e08b236b97a68e7059c1ee
Jesse Watson <jexxe@live.com>
2023-06-24 08:08:39 -0400

    removed debug logging

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -6,7 +6,7 @@ import { loadFull } from "tsparticles";
 
 const Background = () => {
   const particlesInit = useCallback(async (engine: Engine) => {
-    console.log(engine);
+    // console.log(engine);
 
     // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
@@ -16,7 +16,8 @@ const Background = () => {
 
   const particlesLoaded = useCallback(
     async (container: Container | undefined) => {
-      await console.log(container);
+      // conditional loading should happen here.
+      return;
     },
     []
   );
