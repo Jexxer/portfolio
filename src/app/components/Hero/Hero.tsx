a0434140dcae817fba826a650b556b7f6fd60d80
Jesse Watson <jexxe@live.com>
2023-07-03 14:57:14 -0400

    alignment changes to hero

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -11,6 +11,8 @@ const Hero = () => {
         "min-h-[calc(100vh-4rem)] w-full",
         "flex flex-col items-center",
         "relative",
+        "mt-8",
+        "md:mt-12",
         "lg:mt-16"
       )}
     >
@@ -24,9 +26,9 @@ const Hero = () => {
         <h2 className="text-theme-text text-2xl font-bold backdrop-blur-sm">
           Software Engineer
         </h2>
-        <p className="text-theme-text max-w-[586px] backdrop-blur-sm">
+        <p className="text-theme-text max-w-[586px] backdrop-blur-sm text-center">
           I&apos;m a software engineer specialized in the creation and design of
-          exceptional digital experiences Currently, I&apos;m focused on
+          exceptional digital experiences. Currently, I&apos;m focused on
           building automated penetration testing web tools at{" "}
           <span className="text-theme-accent font-bold">
             <Link target="_blank" href="https://ondefend.com/">
